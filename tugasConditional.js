cdc19730e2a1b1266ab508f5095d20d77df900cf
athfal-r <mathfalrizki12@gmail.com>
2021-04-23 01:26:27 +0700

    task switch case

--- tugasConditional.js
+++ tugasConditional.js
@@ -43,3 +43,48 @@ if (nama2 === 'Danu' && peran2 === 'Penyihir'){
     console.log('Nama harus diisi');
 }
 
+//TASK SWITCH-CASE
+
+let tanggal = 5;
+let bulan = 2;
+let tahun = 1945;
+
+switch (bulan){
+    case 1 :
+         console.log(`${tanggal} Januari ${tahun} `);
+    break;
+    case 2 :
+        console.log(`${tanggal} Februari ${tahun} `);
+    break;
+    case 3 :
+        console.log(`${tanggal} Maret ${tahun} `);
+    case 4 :
+        console.log(`${tanggal} April ${tahun} `);
+    break;
+    case 5 :
+         console.log(`${tanggal} Mei ${tahun} `);
+    break;
+    case 6 :
+        console.log(`${tanggal} Juni ${tahun} `);
+    case 7 :
+         console.log(`${tanggal} Juli ${tahun} `);
+    break;
+    case 8 :
+        console.log(`${tanggal} Agustus ${tahun} `);
+    break;
+    case 9 :
+        console.log(`${tanggal} September ${tahun} `);
+    case 10 :
+        console.log(`${tanggal} Oktober ${tahun} `);
+    break;
+    case 11 :
+        console.log(`${tanggal} November ${tahun} `);
+    break;
+    case 12 :
+        console.log(`${tanggal} Desember ${tahun} `);
+    break;
+    default:
+    console.log('Tanggal yang dimasukan salah');
+}
+
+
