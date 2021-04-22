b579560018c2019f14653f26d415ebe631a1977e
syafiq <syafiqardiansyah30@gmail.com>
2021-04-23 01:13:30 +0700

    Tugas conditional Syafiq

---
+++ taskIfElse.js
@@ -0,0 +1,47 @@
+let nama = 'Syafiq';
+let peran = 'Tabib';
+
+
+if(nama){
+    console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
+}else{
+    console.log('Nama harus diisi!');
+}
+if(peran){
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+    console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+}
+
+let nama1 = 'Faris';
+let peran1 = 'Tabib';
+
+console.log('=================================================================');
+
+if(nama1){
+    console.log(`Halo ${nama1}, Pilih peranmu untuk memulai game!`);
+}else{
+    console.log('Nama harus diisi!');
+
+}
+if(peran1){
+    console.log(`Selamat datang di Dunia Proxytia, ${nama1}`);
+    console.log(`Halo Tabib ${nama1}, kamu akan membantu temanmu yang terluka.`);
+}
+
+let nama2 = 'Dimas';
+let peran2 = 'Penyihir';
+
+console.log('=================================================================');
+
+if(nama2){
+    console.log(`Halo ${nama2}, Pilih peranmu untuk memulai game!`);
+}else{
+    console.log('Nama harus diisi!');
+}
+
+if(peran2){
+    console.log(`Selamat datang di Dunia Proxytia, ${nama2}`);
+    console.log(`Halo Zero ${nama2}, ciptakan keajaiban yang membantu kemenanganmu!s`)
+}else{
+    console.log("Isilah peran yang akan kamu pilih!");
+}
