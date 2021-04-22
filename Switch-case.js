0e7a26d13d7af540a4326cbac610f89148db4043
fariz-bd <komjend46@gmail.com>
2021-04-22 18:34:28 +0700

    jawaban tugas

---
+++ Switch-case.js
@@ -0,0 +1,49 @@
+let hari = 21;
+let tanggal = 13;
+let tahun = 2000;
+
+switch (tanggal){
+    case 1 :
+        console.log( "'" + hari + ', Januari, ' + tahun + "';");
+        break;
+    case 2 :
+        console.log( "'" + hari + ', February, ' + tahun + "';");
+        break;
+    case 3 :
+        console.log( "'" + hari + ', Mart, ' + tahun + "';");
+        break;
+    case 4 :
+        console.log( "'" + hari + ', April, ' + tahun + "';");
+        break;
+    case 5 :
+        console.log( "'" + hari + ', Mei, ' + tahun + "';");
+        break;
+    case 6 :
+        console.log( "'" + hari + ', Juny, ' + tahun + "';");
+        break;
+    case 7 :
+        console.log( "'" + hari + ', July, ' + tahun + "';");
+        break;
+    case 8 :
+        console.log( "'" + hari + ', Agustus, ' + tahun + "';");
+        break;
+    case 9 :
+        console.log( "'" + hari + ', September, ' + tahun + "';");
+        break;
+    case 10 :
+        console.log( "'" + hari + ', Oktober, ' + tahun + "';");
+        break;
+    case 11 :
+        console.log( "'" + hari + ',  November, ' + tahun + "';");
+        break;
+    case 12 :
+        console.log( "'" + hari + ',  Desember, ' + tahun + "';");
+        break;
+    default :
+        console.log("error");
+
+}
+
+
+
+
