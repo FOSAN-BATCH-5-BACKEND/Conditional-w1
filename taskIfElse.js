008e4c056a534b2e8e824fe247f5af677633258f
syafiq <syafiqardiansyah30@gmail.com>
2021-04-23 10:56:31 +0700

    Tugas Conditional Syafiq

--- taskIfElse.js
+++ taskIfElse.js
@@ -1,47 +1,20 @@
 let nama = 'Syafiq';
-let peran = 'Tabib';
-
+let peran = 'Ksatria';
 
 if(nama){
     console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
 }else{
     console.log('Nama harus diisi!');
 }
-if(peran){
+if(peran === 'Ksatria'){
     console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
     console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
-}
-
-let nama1 = 'Faris';
-let peran1 = 'Tabib';
-
-console.log('=================================================================');
-
-if(nama1){
-    console.log(`Halo ${nama1}, Pilih peranmu untuk memulai game!`);
+}else if(peran === 'Tabib'){
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+    console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`);
+}else if(peran === 'Penyihir'){
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+    console.log(`Halo Penyihir ${nama}, Cinptakan keajaiban yang membantu kemenanganmu!`);
 }else{
-    console.log('Nama harus diisi!');
-
-}
-if(peran1){
-    console.log(`Selamat datang di Dunia Proxytia, ${nama1}`);
-    console.log(`Halo Tabib ${nama1}, kamu akan membantu temanmu yang terluka.`);
-}
-
-let nama2 = 'Dimas';
-let peran2 = 'Penyihir';
-
-console.log('=================================================================');
-
-if(nama2){
-    console.log(`Halo ${nama2}, Pilih peranmu untuk memulai game!`);
-}else{
-    console.log('Nama harus diisi!');
-}
-
-if(peran2){
-    console.log(`Selamat datang di Dunia Proxytia, ${nama2}`);
-    console.log(`Halo Zero ${nama2}, ciptakan keajaiban yang membantu kemenanganmu!s`)
-}else{
-    console.log("Isilah peran yang akan kamu pilih!");
+    console.log('Peran harus diisi!');
 }
